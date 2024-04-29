9b1a7af70ab305acd64e10e514dae4998516b95c
mintchocowin <sliuy59@gmail.com>
2024-04-29 20:44:22 +0900

    mj

--- stanley/src/component/Navigation.js
+++ stanley/src/component/Navigation.js
@@ -1,3 +1,4 @@
+import "../component/Navigation.css";
 import {
   Container,
   Nav,
@@ -7,9 +8,17 @@ import {
   Button,
 } from "react-bootstrap";
 import styled from "styled-components";
+import logo from "../image/logo.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronDown,
+  faMagnifyingGlass,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
 import usaFlag from "../image/flag-usa.jpg";
+
+const navmenu = ["NEW", "QUENCHER", "HYDRATE", "SHOP", "APPAREL", "CUSTOMIZE"];
+
 const Navigation = () => {
   return (
     <div>
@@ -33,44 +42,16 @@ const Navigation = () => {
         <br />
       </div>
       <div className="nav2">
-        <Navbar expand="lg" className="bg-body-tertiary">
-          <Container fluid>
-            <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
-              >
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Link" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#" disabled>
-                  Link
-                </Nav.Link>
-              </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <div className="nav2-right">
+          <img width={250} src={logo} />
+          {navmenu.map((item, idx) => (
+            <p key={idx}>{item}</p>
+          ))}
+        </div>
+        <div className="nav2-left">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <FontAwesomeIcon icon={faCartShopping} />
+        </div>
       </div>
     </div>
   );
